10e002755b410515a71fcdac046ced4bc4e14481
igor_walace <131620215+Igorwalace@users.noreply.github.com>
2024-04-18 23:21:30 -0300

    Atualizar o divTitle.tsx

--- src/app/pages/componentes/divTitle.tsx
+++ src/app/pages/componentes/divTitle.tsx
@@ -5,7 +5,7 @@ const DivTitle = () => {
         <>
             <h1 className="flex items-center gap-2 capitalize text-4xl">
                 <FaAtlas className="text-[#E9A23B]" size={25} />
-                Meu quadro de tarefas
+                Minhas tarefas
             </h1>
             <p className="text-base ml-9 capitalize">Tarefas para manter organizado</p>
         </>
